6ba4da27a43d2a5f47bcf5cae25905d0bf1c2da9
“Litian <litian_zhang17@163.com>
2021-07-02 00:20:36 +0800

    Load all images according to index file

--- lib/Loader.dart
+++ lib/Loader.dart
@@ -10,12 +10,14 @@ Future<List<String>> readNamesAndYearsFile() async {
   return namesAndYearsList;
 }
 
-Future<void> loadImages() async {
+Future<void> loadImages(BuildContext context) async {
   List<Circle> circles;
   List<String> namesAndYearsList = await readNamesAndYearsFile();
 
   List<String> namesList = [];
   List<String> yearsList = [];
+
+  List<String> imageAvailability = [];
 
   for(String s in namesAndYearsList) {
     namesList.add(s.split(" ")[0]);
@@ -24,10 +26,25 @@ Future<void> loadImages() async {
     String name = s.split(" ")[0];
     String year = s.split(" ")[1];
 
-    Image image = Image.asset('assets/images/' + 'laie_hawaii_temple' + '_large.webp');
+    String imagePath = 'assets/images/' + name + '_large.webp';
 
-    // print(image);
+    try {
+      final bundle = DefaultAssetBundle.of(context);
+      await bundle.load(imagePath);
+      // print('we have this image');
+      imageAvailability.add('1');
+      Image image = Image.asset(imagePath);
+
+    } catch (e) {
+      Image image = Image.asset('assets/images/' + 'no_image' + '_large.webp');
+      print('no image');
+      imageAvailability.add('0');
+    }
+
+  // print(image);
   }
+
+  print("imageAvailability is: " + imageAvailability.toString());
 
   print('names length is: ' + namesList.length.toString());
   // print(namesList);

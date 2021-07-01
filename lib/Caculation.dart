4330523b258b8f2be86fe594f31bff7fcd702185
“Litian <litian_zhang17@163.com>
2021-07-01 20:06:24 +0800

    Draw spiral

---
+++ lib/Caculation.dart
@@ -0,0 +1,33 @@
+import 'dart:math';
+
+
+List<List<double>> getCoordinatesAndSizes() {
+    double initialRadius = 0.1;
+    double centerX = 0.5;
+    double centerY = 0.5;
+
+    List<List<double>> coordinatesAndSizes = <List<double>>[];
+
+    for (double i = -18.0; i < 17.5; i += 0.02) {
+      double x = centerX + initialRadius * (exp(i * 1 / (tan(47 * pi / 100)))) * (cos(i));
+      double y = centerY + initialRadius * (exp(i * 1 / (tan(47 * pi / 100)))) * (sin(i));
+
+      List<double> temp = [x, y];
+      coordinatesAndSizes.add(temp);
+    }
+
+    double topCoordinateX = coordinatesAndSizes.last.first;
+    double topCoordinateY = coordinatesAndSizes.last.last;
+
+    for (double i = 0; i < 20; i += 1) {
+      List<double> temp = [];
+      temp.add(topCoordinateX + i * 20);
+      temp.add(topCoordinateY);
+      coordinatesAndSizes.add(temp);
+    }
+
+    return coordinatesAndSizes;
+
+  }
+
+
---
+++ lib/MyHomePage.dart
@@ -0,0 +1,141 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+// import 'dart:ui';
+
+import 'package:spiral_vis/Caculation.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  // var w = window.physicalSize.width;
+  // var h = window.physicalSize.height;
+
+  var large = true;
+
+  var sampleW = 100.0;
+
+  List<List<double>> coordinatesAndSizes =[];
+
+
+
+
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    coordinatesAndSizes = getCoordinatesAndSizes();
+
+  }
+
+  Stack buildSpiral(BoxConstraints constraints) {
+
+    print(constraints.maxWidth);
+    print(constraints.maxHeight);
+
+    List<Positioned> points = [];
+
+    for(List<double> i in coordinatesAndSizes) {
+      Positioned point = Positioned(
+          top: i.first * constraints.maxWidth,
+          left: i.last * constraints.maxWidth,
+          // left: w/2,
+          child: Container(
+            width: 10,
+            height: 10,
+            color: Colors.black,
+          )
+      );
+
+      points.add(point);
+
+    }
+
+    Stack stack = Stack(children: points,);
+
+    print('coordinates and sizes length: ' + coordinatesAndSizes.length.toString());
+    print('stack children length: ' + stack.children.length.toString());
+
+    return stack;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return
+      Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [
+
+        ],
+      ),
+      body: LayoutBuilder (
+        builder: (context, constraints) {
+          return
+            buildSpiral(constraints);
+
+            Stack(
+            children: [
+              Positioned(
+                top: 0.2 * constraints.maxHeight,
+                left: 0.2 * constraints.maxWidth,
+                // left: w/2,
+                child: AnimatedContainer(
+                  duration: Duration(milliseconds: 1000),
+                  color: Colors.red,
+                  width: sampleW,
+                  child: Image.asset(
+                    "assets/images/laie_hawaii_temple_large.webp",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+              Positioned(
+                top: 0.1 * constraints.maxHeight,
+                // bottom: 0.2 * constraints.maxHeight,
+                left: 0.1 * constraints.maxWidth,
+                // right: 0.2 * constraints.maxWidth,
+                // left: w/2,
+                child: TextButton(
+                  onPressed: () {
+                    // print(w);
+                    // print(h);
+                    print(constraints.maxWidth);
+                    print(constraints.maxHeight);
+                    print(coordinatesAndSizes.length);
+                    print(coordinatesAndSizes);
+                    setState(() {
+                      large = !large;
+                      sampleW = sampleW + 100;
+                    });
+                  },
+                  child: const Text('button', style: TextStyle(color: Colors.red),),
+                ),
+              ),
+
+
+
+            ],
+          );
+        }
+
+      ),
+
+
+     // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}

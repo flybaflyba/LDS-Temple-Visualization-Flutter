8638d9ebcb34521d56ac6eeff963f2d59768d893
“Litian <litian_zhang17@163.com>
2021-07-02 09:00:27 +0800

    Format real names

--- lib/Loader.dart
+++ lib/Loader.dart
@@ -14,17 +14,23 @@ Future<void> loadImages(BuildContext context) async {
   List<Circle> circles;
   List<String> namesAndYearsList = await readNamesAndYearsFile();
 
-  List<String> namesList = [];
-  List<String> yearsList = [];
+  // List<String> namesList = [];
+  // List<String> yearsList = [];
 
-  List<String> imageAvailability = [];
+  List<bool> imageAvailability = [];
 
   for(String s in namesAndYearsList) {
-    namesList.add(s.split(" ")[0]);
-    yearsList.add(s.split(" ")[1]);
+    // namesList.add(s.split(" ")[0]);
+    // yearsList.add(s.split(" ")[1]);
 
     String name = s.split(" ")[0];
     String year = s.split(" ")[1];
+
+    var realName = '';
+    for(String s in name.split("_")){
+      realName = realName + '${s[0].toUpperCase()}${s.substring(1)}' + " ";
+    }
+    print(realName);
 
     String imagePath = 'assets/images/' + name + '_large.webp';
 
@@ -32,24 +38,27 @@ Future<void> loadImages(BuildContext context) async {
       final bundle = DefaultAssetBundle.of(context);
       await bundle.load(imagePath);
       // print('we have this image');
-      imageAvailability.add('1');
+      imageAvailability.add(true);
       Image image = Image.asset(imagePath);
 
     } catch (e) {
       Image image = Image.asset('assets/images/' + 'no_image' + '_large.webp');
-      print('no image');
-      imageAvailability.add('0');
+      // print('no image');
+      imageAvailability.add(false);
     }
-
   // print(image);
   }
 
-  print("imageAvailability is: " + imageAvailability.toString());
+  // print("imageAvailability is: " + imageAvailability.toString());
 
-  print('names length is: ' + namesList.length.toString());
-  // print(namesList);
-  print('yearsList length is: ' + yearsList.length.toString());
-  // print(yearsList);
+  // print('names length is: ' + namesList.length.toString());
+  // // print(namesList);
+  // print('yearsList length is: ' + yearsList.length.toString());
+  // // print(yearsList);
+  //
+  // var temp = namesList[100].split(" ").first;
+  // print(temp);
+
 
 
 }

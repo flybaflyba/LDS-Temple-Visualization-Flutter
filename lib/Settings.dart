bc36b0dd4b1940b8f51318a8aaa1d973885f324f
“Litian <litian_zhang17@163.com>
2021-07-08 16:24:37 +0800

    Add todo notes

--- lib/Settings.dart
+++ lib/Settings.dart
@@ -26,5 +26,6 @@ class _SettingsState extends State<Settings>{
     in case some un-relevant photos to show up, eg for shanghai temple
     2. change initial loading to top bar, load spiral with blank circles
     so that users can interact with spiral while images are still loading
+    3. fix refreshing adding extra names and years to picker lists, for both names and years.
 
  **/

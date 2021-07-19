018165071293f7a7732295fbe590091876633bc7
“Litian <litian_zhang17@163.com>
2021-07-19 17:35:19 +0800

    Update todo

--- lib/Settings.dart
+++ lib/Settings.dart
@@ -22,10 +22,8 @@ class _SettingsState extends State<Settings>{
 }
 
 /**
- * TODO 1. change see more photos on line on single view search term to include 'lds'
-    in case some un-relevant photos to show up, eg for shanghai temple
-    2. change initial loading to top bar, load spiral with blank circles
-    so that users can interact with spiral while images are still loading
-    3. fix refreshing adding extra names and years to picker lists, for both names and years.
+ * TODO
+    1. fix refreshing adding extra names and years to picker lists, for both names and years.
+    2. https://pub.dev/packages/gzx_dropdown_menu
 
  **/
